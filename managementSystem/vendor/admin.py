b4062587e53ed3715152d6b4deb1637174d08292
shabbir-zinnox <sk.mohammedshabbir@gmail.com>
2023-11-28 10:41:58 +0530

    imporved admin panel, modified setting for jwt

--- managementSystem/vendor/admin.py
+++ managementSystem/vendor/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import *
 
+
+admin.site.site_header = 'VMS admin'
+admin.site.site_title = 'VMS admin'
+admin.site.index_title = 'Vendor Management administration'
+admin.empty_value_display = '**Empty**'
+
+
+
+@admin.register(Vendor)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("name","vendor_code")
+    list_filter = ("vendor_code",)
+    search_fields = ("vendor_code",)
+
+
+@admin.register(PurchaseOrder)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("po_number","vendor",)
+    list_filter = ("po_number",)
+    search_fields = ("po_number",)
+
+
+@admin.register(HistoricalPerformance)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("vendor","quality_rating_avg",)
+    list_filter = ("vendor",)
+    search_fields = ("vendor",)
+
 admin.site.register(User)
-admin.site.register(Vendor)
-admin.site.register(PurchaseOrder)
-admin.site.register(HistoricalPerformance)
